7b9ca8d9b06930b6e939a5d1349fba4d3e357a58
Ahmed Hesham Fayed <111004537+AhmedFayed123@users.noreply.github.com>
2023-01-02 15:10:53 +0200

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	
+	int width,length,area ;
+	
+	
+	cout<<" please enter rectangle width : ";
+	cin>>width ;
+	
+	cout<<" please enter rectangle length : ";
+	cin>>length ;
+	
+	area = width * length ;
+	cout<<"area of rectangle equal "<< area <<"\n" ;
+	
+	
+	
+	
+	return 0;
+	
+}
